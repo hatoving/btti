c1f4753154d6b6644ff00607789b44c736c697d3
Attila Kuti <64536760+hatoving@users.noreply.github.com>
2025-08-23 20:35:43 +0200

    Added 2 new boosters

---
+++ items/boosters.lua
@@ -0,0 +1,77 @@
+-- Buffoon Packs
+
+-- Legendary Joker Pack
+SMODS.Atlas {
+    key = "legendaryBP",
+    path = "bttiLegendaryBuffoonPack.png",
+    px = 71,
+    py = 95
+}
+SMODS.Booster {
+    key = "buffoon_legendary",
+    weight = 0.6,
+    kind = 'Buffoon', -- You can also use Buffoon if you want it to belong to the vanilla kind
+    cost = 100,
+    pos = { x = 0, y = 0 },
+    atlas = 'legendaryBP',
+    config = { extra = 2, choose = 1 },
+    group_key = "k_buffoon_pack",
+    rarity = 4,
+    loc_txt = {
+        name = "Legendary Buffoon Pack",
+        text = {
+            "Choose 1 of up to 2",
+            "{C:attention}Legendary Jokers"
+        },
+    },
+    loc_vars = function(self, info_queue, card)
+        return {
+        }
+    end,
+    ease_background_colour = function(self)
+        ease_background_colour_blind(G.STATES.BUFFOON_PACK)
+    end,
+    create_card = function(self, card, i)
+        local c = create_card('Joker', G.pack_cards, true, 4, true, true, nil, 'BuffoonLen')
+        return c
+    end,
+}
+
+SMODS.Atlas {
+    key = "fortuneBP",
+    path = "bttiFortuneBuffoonPack.png",
+    px = 71,
+    py = 95
+}
+SMODS.Booster {
+    key = "buffoon_fortune",
+    weight = 0.6,
+    kind = 'Buffoon', -- You can also use Buffoon if you want it to belong to the vanilla kind
+    cost = 30,
+    pos = { x = 0, y = 0 },
+    atlas = 'fortuneBP',
+    config = { extra = 2, choose = 1 },
+    group_key = "k_buffoon_pack",
+    rarity = 4,
+    loc_txt = {
+        name = "Fortune Buffoon Pack",
+        text = {
+            "Choose 1 of up to 2",
+            "{C:attention}Common/Uncommon Jokers{}",
+            "with a random {C:dark_edition}Edition{}."
+        },
+    },
+    loc_vars = function(self, info_queue, card)
+        return {
+        }
+    end,
+    ease_background_colour = function(self)
+        ease_background_colour_blind(G.STATES.BUFFOON_PACK)
+    end,
+    create_card = function(self, card, i)
+        local randomEdition = poll_edition("btti", nil, false, true)
+        local c = SMODS.create_card( { key = pseudorandom_element(G.P_JOKER_RARITY_POOLS[math.random(1, 2)], pseudoseed('btti')).key })
+        c:set_edition(randomEdition)
+        return c
+    end,
+}
