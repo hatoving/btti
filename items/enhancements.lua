3609d150805930d7fcfc546699fd0c5c5c8b232e
Attila Kuti <64536760+hatoving@users.noreply.github.com>
2025-08-25 21:45:10 +0200

    added spilled card with no functionality yet

--- items/enhancements.lua
+++ items/enhancements.lua
@@ -24,3 +24,32 @@ SMODS.Enhancement {
         return { vars = { card.ability.bonus } }
     end,
 }
+
+-- Spilled Card
+SMODS.Atlas {
+    key = "spilledCard",
+    path = "bttiSpilledCard.png",
+    px = 71,
+    py = 95
+}
+SMODS.Enhancement {
+    key = 'spilledCard',
+    loc_txt = {
+        name = 'Spilled Card',
+        text = {
+            "{C:green}1 in 2{} to leak into the",
+            "card to its right, triggering it",
+            "once before that card is triggered"
+        }
+    },
+    atlas = 'spilledCard',
+    pos = { x = 0, y = 0 },
+    config = { },
+    replace_base_card = false,
+    no_rank = true,
+    no_suit = true,
+    loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue + 1] = { key = 'bttiFromWhere', set = 'Other', vars = { "You're My Favorite Person" } }
+        return { vars = { } }
+    end,
+}
