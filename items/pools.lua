c8189283d4617ffc797c58056ab30c4c0c903d5f
Attila Kuti <64536760+hatoving@users.noreply.github.com>
2025-08-31 06:04:50 +0200

    Gaming

---
+++ items/pools.lua
@@ -0,0 +1,85 @@
+--#region Pool creation
+
+SMODS.ObjectType({
+    key = "BTTI_modAddition",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_COMBO",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_planets",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_tarots",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_spectrals",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_ITTI",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_SMP",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_DEETS",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+SMODS.ObjectType({
+    key = "BTTI_modAddition_DEETSPlanets",
+    default = "j_reserved_parking",
+    cards = {},
+    inject = function(self)
+        SMODS.ObjectType.inject(self)
+        -- insert base game food jokers
+    end,
+})
+
+--#endregion
