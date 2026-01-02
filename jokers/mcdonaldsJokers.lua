7b2aeeec4b456d57cd61cf9b5ab8e44b095d854d
BlueBen8 <funkyben01@gmail.com>
2026-01-01 22:21:29 -0500

    mcdonald fry

--- jokers/mcdonaldsJokers.lua
+++ jokers/mcdonaldsJokers.lua
@@ -16,7 +16,7 @@ SMODS.Joker {
 		}
 	},
 
-	config = { extra = { mult = 10, odds = 10 } },
+	config = { extra = { mult = 10 } },
 	loc_vars = function(self, info_queue, card)
         local combinable = G.BTTI.getCombinableJokers(card.ability.name)
         for _, line in ipairs(combinable) do
@@ -26,7 +26,7 @@ SMODS.Joker {
                 vars = { line }
             }
         end
-        info_queue[#info_queue + 1] = { key = 'bttiFromBy', set = 'Other', vars = { "McDonald's", "Ronald McDonald" } }
+        info_queue[#info_queue + 1] = { key = 'bttiFromBy', set = 'Other', vars = { "McDonald's", "Ronald McDonald", "BlueBen8"} }
 		return {
             vars = { },
         }
@@ -44,8 +44,22 @@ SMODS.Joker {
     perishable_compat = false,
 
 	calculate = function(self, card, context)
-		-- TO DO
-	end,
+		if context.joker_main then
+            return {
+                mult = card.ability.extra.mult
+            }
+        end
+        if context.after then
+            if pseudorandom('McFries') < G.GAME.probabilities.normal then
+                local rand = pseudorandom("btti_" .. card.ability.name, 0, 5)
+                return {
+                    mult = rand,
+                    mult_mod = card.ability.extra.mult,
+                    message = "bag fries!"
+                }
+            end
+	end
+end,
     in_pool = function(self, args)
 		return false, { allow_duplicates = false }
 	end
